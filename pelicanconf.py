d0f541d75f4467cac53552aacf6a0bec3f1bdac9
Alex Dong <me@alexdong.com>
2018-12-26 01:02:56 +1300

    migrate to Pelican

---
+++ pelicanconf.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Alex Dong <me@alexdong.com>'
+SITENAME = 'Raw thoughts from Alex Dong'
+SITEURL = ''
+THEME = 'alexdong'
+PATH = 'content'
+
+TIMEZONE = 'Pacific/Auckland'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
